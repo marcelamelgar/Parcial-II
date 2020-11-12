c6a99fa89ee11e4797a686b19f08fa570e9d054c
marcelamelgar <61280126+marcelamelgar@users.noreply.github.com>
2020-11-11 20:31:03 -0600

    corregido

--- app.py
+++ app.py
@@ -7,38 +7,77 @@ env = Environment(loader = file_loader)
 
 
 app = Flask(__name__)
-with open('data.json') as json_file:
-    jsonfile = json.load(json_file)
 
 
 def mensaje ():
     mensaje = 'Web String Handler'
-    return "alert('" + mensaje + "')"
+    return mensaje
 
-@app.route('/')
+def reverse(palabra: str):
+    reversedS = palabra[::-1]
+    return reversedS
+
+def length(palabra: str):
+    return len(palabra)
+
+def vowels(palabra: str):
+    vowel = "AaEeIiOoUu"
+    return (len([letter for letter in palabra if letter in vowel]))
+
+def consonants(palabra: str):
+    consonants = "BbCcDdFfGgHhJjKkLlMmNnPpQqRrSsTtVvWwXxYyZz"
+    return (len([letter for letter in palabra if letter in consonants]))
+
+def upper(palabra: str):
+    return palabra.upper()
+
+def lower(palabra: str):
+    return palabra.lower()
+
+def updown(palabra: str):
+    contador = 0
+    upalabra =  ""
+    for i in palabra:
+        if contador % 2 == 0:
+            upalabra = upalabra + i.lower()
+        else:
+            upalabra = upalabra + i.upper()
+        contador += 1
+    return upalabra
+    
+
+def naive(palabra: str):
+    newpalabra = ""
+    newpalabra = palabra.replace("a", "@").replace("e", "3").replace("i", "!").replace("o", "0").replace("u", ")")
+    return newpalabra
+
+def handler(palabra: str):
+    outcome = {}
+    if palabra == "":
+        return outcome
+    outcome["Reversed"] = reverse(palabra)
+    outcome["Length"] = length(palabra)
+    outcome["Vowels"] = vowels(palabra)
+    outcome["Consonants"] = consonants(palabra)
+    outcome["Upper"] = upper(palabra)
+    outcome["Lower"] = lower(palabra)
+    outcome["Updown"] = updown(palabra)
+    outcome["Naive"] = naive(palabra)
+    return outcome
+
+ 
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-    template = env.get_template('handler.html')
-    picture = url_for('static', filename = jsonfile['fotografia'])
-    return template.render(torender = jsonfile['Render'], palabra = ['Word'], picture = picture)
-
-@app.route('/new', methods = ['GET', 'POST'])
-def nueva():
+    resultado = {}
     if request.method == 'POST':
-        _string = request.form['String']
-        _length = request.form['Length']
-        _vowels = request.form['Vowels']
-        _consonants = request.form['Consonants']
-        _upper = request.form['Upper']
-        _lower = request.form['Lower']
-        _updown = request.form['UpDown']
-        _naive = request.form['Naive']
-        print (f'{_string}{_length}{_vowels}{_consonants}{_upper}{_lower}{_updown}{_naive}')
+        string_arg = request.form['String']
+        print(f"String a manipular {string_arg}")
+        resultado = handler(string_arg)
+        print(f"diccionario a manipular{resultado}")
 
-        jsonfile['Render'].append({"Length": _length}, {"Vowels":_vowels}, {"Consonants": _consonants}, {"Upper": _upper}, {"Lower": _lower}, {"UpDown":_updown}, {"Naive": _naive})
-        jsonfile['Word'].append({"String": _string})
-        return redirect(url_for('handler'))
-    template = env.get_template('form.html')
-    return template.render()
+    template = env.get_template('handler.html')
+
+    return template.render(resultado = resultado)
 
 
 if __name__ == "__main__":
